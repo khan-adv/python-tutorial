ae0f504537aedebca5cedd5fddb24b7091eaf178
Manglu-1 <yaseen262006@gmail.com>
2025-08-02 18:36:12 +0530

    second commit by my bro

---
+++ Password block systum.py
@@ -0,0 +1,22 @@
+password = ""
+while password != "MangluBadmas":
+    password = input("Enter your password")
+    if password == "MangluBadmas":
+        print("Access Granted")
+        break
+    else:
+        print("Wrong password ,Try again!")
+
+password = "MangluBadmas"
+attempts = 0
+while attempts <3:
+    user_input = input("Enter your password")
+    if user_input == password:
+        print("Access Granted ✅")
+        break
+    else:
+        attempts +=1
+        print(f"Wrong password Attempts left: {3-attempts}" )
+
+    if attempts ==3:
+        print("❌Too many wrong attempts , Account Blocked!")
